48f6e327766bcd86e3cdcf4f61c3f84f7f4af093
23matsunamine23 <noname0323namine@gmail.com>
2019-10-04 20:52:18 +0900

    create new action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,4 +4,11 @@ class MessagesController < ApplicationController
     @messages = Message.all
   end
 
+  def new
+  end
+
+  def create
+    Message.create(text: "" )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages' => 'messages#index'
+  get 'messages' => 'messages#index'  #メッセージ投稿画面
+  get 'messages/new' => 'messages#new'  #メッセージ投稿画面
+
 end
