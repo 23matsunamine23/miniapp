89bf4afb8c2db4d0a9f451fd1a52ac95add9f947
23matsunamine23 <noname0323namine@gmail.com>
2019-10-05 15:36:15 +0900

    create messages/new.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages' => 'messages#index'  #メッセージ投稿画面
   get 'messages/new' => 'messages#new'  #メッセージ投稿画面
-
+  post 'messages' => 'messages#create'  #メッセージ投稿機能
 end
