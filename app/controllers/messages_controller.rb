6dcfb438669306cf1405b21d2673153f14bf439c
23matsunamine23 <noname0323namine@gmail.com>
2019-10-06 16:53:17 +0900

    create destroy,edit,update

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,20 +1,45 @@
 class MessagesController < ApplicationController
 
+  before_action :move_to_index, except: :index
+
   def index
-    @messages = Message.all.order("id DESC")
+    @messages = Message.all.order("created_at DESC").page(params[:page]).per(5)
   end
 
   def new
   end
 
   def create
-    Message.create(message_params)
+    Message.create(text: message_params[:text], user_id: current_user.id)
     redirect_to action: :index
   end
+
+  def destroy
+    message = Message.find(params[:id])
+    message.destroy if message.user_id == current_user.id
+    redirect_to action: :index
+  end
+
+  def edit
+    @message = Message.find(params[:id])
+  end
+
+  def update
+    message = Message.find(params[:id])
+    if message.user_id == current_user.id
+      message.update(message_params)
+    end
+    redirect_to action: :index
+  end
+
 
   private
   def message_params
     params.permit(:text)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get '/messages' => 'messages#index'  #メッセージ投稿画面
   get '/messages/new' => 'messages#new'  #メッセージ投稿画面
   post '/messages' => 'messages#create'  #メッセージ投稿機能
+  delete  'messages/:id'  => 'messages#destroy'  #メッセージ削除機能
+  get   'messages/:id/edit'  => 'messages#edit'  #メッセージ編集画面
+  patch 'messages/:id' => 'messages#update'  #メッセージ編集機能
 end
